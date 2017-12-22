df293495878574e2c1b2f6ac33271438bdfa6bc9
Panthson <p10a20w98@gmail.com>
2017-12-21 22:08:43 -0800

    Updated GUI to have classes and moved appropriate parts to main file

--- InstaGrab.py
+++ InstaGrab.py
@@ -1,5 +1,11 @@
+import tkinter
 import InstaGui as gui
 
-#def main():
+def main():
+    window = tkinter.Tk()
+    window.resizable(width=False, height=False)
+    download = gui.InstaGui(window)
+    window.mainloop()
 
-#if __name__ == '__main__':
+if __name__ == '__main__':
+    main()
--- InstaGui.py
+++ InstaGui.py
@@ -2,36 +2,34 @@ from tkinter import *
 import GetURLs as urls
 import DownloadPictures as dp
 
-def getUser():
-    urls.getURLs(user.get())
+class InstaGui:
 
-window = Tk()
-#window.minsize(width=300, height=100)
-window.resizable(width=False, height=False)
+    def __init__(self, master):
+        mainFrame = Frame(master, padx="10px", pady="5px")
+        titleFrame = Frame(mainFrame)
+        inputFrame = Frame(mainFrame)
 
-mainFrame = Frame(window, padx="10px", pady="5px")
-titleFrame = Frame(mainFrame)
-inputFrame = Frame(mainFrame)
-
-mainFrame.grid()
-titleFrame.grid(row=0)
-inputFrame.grid(row=1)
+        mainFrame.grid()
+        titleFrame.grid(row=0)
+        inputFrame.grid(row=1)
 
 
-#Create and insert title
-title = Label(titleFrame,
-    text="InstaGrab",
-    font=("System", 20),
-    pady="5px")
-title.grid()
-#Create and insert input field
-#Create and insert search button
-user = Entry(inputFrame)
-searchButton = Button(inputFrame, text="Steal Photos!", command=getUser)
-error = Label(inputFrame, text="User Not Found!", font="System", fg="red")
+        #Create and insert title
+        title = Label(titleFrame,
+            text="InstaGrab",
+            font=("System", 20),
+            pady="5px")
+        title.grid()
+        #Create and insert input field
+        #Create and insert search button
+        global user
+        user = Entry(inputFrame)
+        searchButton = Button(inputFrame, text="Steal Photos!", command=self.getUser)
+        error = Label(inputFrame, text="User Not Found!", font="System", fg="red")
 
-searchButton.grid(row=0, column=1)
-user.grid(row=0, column=0)
-error.grid(row=1, columnspan=2)
+        searchButton.grid(row=0, column=1)
+        user.grid(row=0, column=0)
+        error.grid(row=1, columnspan=2)
 
-window.mainloop()
+    def getUser(self):
+        urls.getURLs(user.get())
